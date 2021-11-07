cd4e4bf61e0a7d5f4fde29d998e5adb75ccbf4c0
Mohammad Oumeir Rengony <oumeir.rengony@hangarww.com>
2021-11-07 21:39:34 +0400

    Added categories

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Link from "next/link";
-
-const categories = [
-  { name: "React", slug: "react" },
-  { name: "web Development", slug: "web-dev" },
-];
+import { getCategories } from "../services";
 
 const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories));
+  }, []);
+  
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
@@ -19,15 +21,13 @@ const Header = () => {
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
-            {
-                categories.map(category => (
-                    <Link key={category.slug} href={`/category/${category.slug}`}>
-                        <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
-                            {category.name}
-                        </span>
-                    </Link>
-                ))
-            }
+          {categories.map((category) => (
+            <Link key={category.slug} href={`/category/${category.slug}`}>
+              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                {category.name}
+              </span>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
